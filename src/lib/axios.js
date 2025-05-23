69c00e6ab48eb6ebe36fdcb4c09f066fbbae5271
Aasif Azlan <aasifazlaan@gmail.com>
2025-05-24 02:24:20 +0530

    axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api=axios.create({
     baseURL:import.meta.env.DEV
 		? "http://localhost:5000/api"
-		: "https://mern-ecom-backend-oay9.onrender.com/api",
+		: "https://aasifazlan-server.onrender.com/api",
 })
 
 export default api;
